239d6bac40bd378b0b2bbdf93024083d3b90817c
Anthony Sena <asena5@its.jnj.com>
2017-08-21 11:17:31 -0400

    Fixing Y_NUM_PERSONS for export

--- inst/sql/sql_server/export_v5/measurement/sqlFrequencyDistribution.sql
+++ inst/sql/sql_server/export_v5/measurement/sqlFrequencyDistribution.sql
@@ -1,6 +1,6 @@
 select c1.concept_id as CONCEPT_ID, 
 	c1.concept_name as CONCEPT_NAME,
-	1.0*num.count_value / denom.count_value as Y_NUM_PERSONS,
+	cast(round((100.0*num.count_value / denom.count_value), 0) as int) as Y_NUM_PERSONS,
 	num.stratum_2 as X_COUNT
 from 
 	(select count_value from @results_database_schema.ACHILLES_results where analysis_id = 1) denom,
